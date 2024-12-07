bacc14633eb2d4998a00655f66dc7748522a2481
Goodkid01 <zaikinbkmkop@gmail.com>
2024-12-07 14:49:08 +0200

    1

--- main.cpp
+++ main.cpp
@@ -1,84 +1,19 @@
-#include <iostream>
-#include <climits>
-#include <cfloat>
-#include <iomanip>
-#include <cmath>
-
+#include "func.hpp"
 
 int main()
 {
-  const int Nmax=3;
-  const int Mmax=3;
-  int matrix[Nmax][Mmax];
-  int amin=1000000;
-  int sum=0;
-  int n;
-  std::cin>> n;
-  for(int i=0; i<n; i++)
-    for(int j=0;j<n; j++)
-  {
-    std::cin >> matrix[i][j];
-  }
-
-for(int i=0; i<n; i++)
-    for(int j=0;j<n; j++)
-  {
-    if(matrix[i][j]<amin)
-      amin=matrix[i][j];
-  }
-
-for(int i=0; i<n; i++)
-    for(int j=0;j<n; j++)
-  {
-    if(matrix[i][j]==amin)
-      sum+=1;
-  }
-int s=0;
-for(int i=0; i<n; i++)
-    for(int j=0;j<n; j++)
-  { int a=abs(matrix[i][j]);
-    for(int i=2; i<=a; i++)
-    {
-      if(a%i==0)
-      {
-        continue;
-      }
-      else
-      s+=1;
-
-    }
-  }
-std::cout<<s<<std::endl;
-if(sum==2 && s>=2)
-{
-  int a=1;
-  int b=1;
-  for(int i=0; i<n-1; i++)
-  {
-    for(int j=0;j<n; j++)
-      {
-        a=a*matrix[i][j];
-        b=b*matrix[i+1][j];
-      
-    if(a<b)
-    {
-      int tmp=matrix[i][j];
-      matrix[i][j]=matrix[i+1][j];
-      matrix[i+1][j]=tmp;
-    }
-      }
-  }
-}
-
-
-  for(int i=0; i<n; i++)
-  {
-    for(int j=0;j<n; j++)
-  {
-    std::cout<<matrix[i][j]<<" ";
-      std::cout<< std::endl;
-  }
-  
-  }
-
+    int n;
+    int count_mini = 1;
+    int count_prime = 0;
+    int matrix[N_MAX][N_MAX];
+    int mas_multi[N_MAX];
+    cin >> n;
+    Read(matrix, n, count_mini, count_prime);
+    if((count_mini == 2) && (count_prime >= 2))
+        {
+            get_multi(matrix, n, mas_multi);
+            Processing(matrix, n, mas_multi);         
+        }
+    Write(matrix, n);
+    return 0;
 }
